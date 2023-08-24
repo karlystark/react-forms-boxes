750282ec7ccc9842ecec3f4d9a3062c9226610f0
Tichin Wu <twu38@mail.ccsf.edu>
2023-08-24 12:06:10 -0700

    WIP first part(before code review)

--- src/Box.js
+++ src/Box.js
@@ -1,6 +1,19 @@
 
-function Box({ backgroundColor, width, height}){
-// display a div with a background color, width, and height based on props sent
+function Box({ id, backgroundColor, width, height, removeBox }) {
+  // display a div with a background color, width, and height based on props sent
+
+  const style = {
+    width: `${width}px`,
+    height: `${height}px`,
+    backgroundColor: backgroundColor
+  };
+  return (
+    <div>
+      <div style={style}>
+      </div>
+      <button onClick={() => removeBox(id)}>X</button>
+    </div>
+  );
 
 }
 
--- src/BoxList.js
+++ src/BoxList.js
@@ -1,17 +1,42 @@
+import React, { useState } from 'react';
+import Box from './Box';
+import NewBoxForm from './NewBoxForm';
+import { v4 as uuid } from 'uuid';
+
+function BoxList() {
+
+  const [boxes, setBoxes] = useState([]);
 
 
-function BoxList(){
-
-  function addBox(formData){
+  function addBox(formData) {
     //creates a new Box given the formData sent from newBoxForm
+    let newBox = { ...formData, id: uuid() };
+    setBoxes(currBoxes => [...currBoxes, newBox]);
   }
 
-// state that contains all of the boxes
-// should render all of the box components (pass in backgroundColor, width, height)
-// along with the NewBoxForm component
-// with each box component, add a button with the text "X" next to each Box.
-    // when "X" is clicked, remove that specific box
-    // pass the "remove" function down to Box as props (button will be included in Box component)
+  function removeBox(id) {
+    setBoxes(boxes => boxes.filter(box => box.id !== id));
+  }
+
+  return (
+    <div>{boxes.map(box => <Box
+      key={box.id}
+      id={box.id}
+      height={box.height}
+      width={box.width}
+      backgroundColor={box.backgroundColor}
+      removeBox={removeBox}
+    />)}
+      <NewBoxForm addBox={addBox} />
+    </div>
+
+  );
+  // state that contains all of the boxes
+  // should render all of the box components (pass in backgroundColor, width, height)
+  // along with the NewBoxForm component
+  // with each box component, add a button with the text "X" next to each Box.
+  // when "X" is clicked, remove that specific box
+  // pass the "remove" function down to Box as props (button will be included in Box component)
 
 }
 
